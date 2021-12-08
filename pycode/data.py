b548f602cea73bace540654c76886137202f2e7c
A. Santhosh Kumar <58947968+Santhoshstark06@users.noreply.github.com>
2021-12-08 20:07:56 +0530

    Update data.py

--- pycode/data.py
+++ pycode/data.py
@@ -7,3 +7,17 @@ import matplotlib.pyplot as plt
 %matplotlib inline
 iris = pd.read_csv("Iris.csv")
 iris.head()
+iris.info()
+iris.drop("Id", axis=1, inplace = True)
+
+fig = iris[iris.Species == 'Iris-setosa'].plot(kind='scatter', x='SepalLengthCm', y='SepalWidthCm', color='orange', label='Setosa')
+iris[iris.Species == 'Iris-versicolor'].plot(kind='scatter', x='SepalLengthCm', y='SepalWidthCm', color='blue', label='Versicolor', ax=fig)
+iris[iris.Species == 'Iris-virginica'].plot(kind='scatter', x='SepalLengthCm', y='SepalWidthCm', color='green', label='Virginica', ax=fig)
+
+fig.set_xlabel('Sepal Length')
+fig.set_ylabel('Sepal Width')
+fig.set_title('Sepal Length Vs Width')
+
+fig=plt.gcf()
+fig.set_size_inches(10, 7)
+plt.show()
