5bd257910b0dcf52e339eb5523ec4985e0f71fe2
A. Santhosh Kumar <58947968+Santhoshstark06@users.noreply.github.com>
2021-12-08 20:07:18 +0530

    Update data.py

--- pycode/data.py
+++ pycode/data.py
@@ -1,1 +1,9 @@
+import numpy as np
+import pandas as pd 
+from pandas import Series, DataFrame
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+iris = pd.read_csv("Iris.csv")
+iris.head()
